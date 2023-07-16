82d94cf2010dc849ebf16a6d18ddfe1a4a9280b9
PandiDurai010722 <pandidurai9009@gmail.com>
2023-07-16 21:57:11 +0530

    completed 3

--- php-2.php
+++ php-2.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
-echo "hallo pandi";
-
-?>
-  
+    $characterfName= "Pandi";
+    $characterAge =29;
+    echo "There onece was a man named $characterfName <br>";
+    echo "He was $characterAge years old<br>";
+    $characterfName="Durai";
+    echo "He really liked the name $characterfName<br>";
+    echo "But didn't like being $cahracterAge<br> "
+    ?>
+</body>
+</html>
---
+++ php-3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Data Types -->
+    <?php
+      $phrase ="To be or not to be ";
+      $age =40; 
+      $gpa = 32.9;
+      $isMale =false;
+      null;
+       
+    ?>
+</body>
+</html>
